0f7b84d5989d74eec49317655051dc32ced1e67b
Panev-Blago <Panev.Blago@gmail.com>
2016-01-27 01:52:36 +0200

    Homework 2

---
+++ Homework2/Task1.java
@@ -0,0 +1,14 @@
+/** Да се изведат на екрана числата от 1 до 100. */
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		byte a = 1;
+		while (a <= 100) {
+			System.out.println(a);
+			a++;
+		}
+
+	}
+
+}
